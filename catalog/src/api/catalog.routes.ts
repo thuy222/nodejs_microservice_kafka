00e847806951113fa6992d7f98614fb44b4ee794
luke <luke.ho@kulu.com>
2024-05-20 22:52:55 +0700

    product dto

--- catalog/src/api/catalog.routes.ts
+++ catalog/src/api/catalog.routes.ts
@@ -1,6 +1,8 @@
 import express, { NextFunction, Request, Response } from "express";
 import { CatalogService } from "../services/catalog.service";
-import { CatalogRepository } from "../repository";
+import { CatalogRepository } from "../repository/catalog.repository";
+import { RequestValidator } from "../utils/requestValidator";
+import { CreateProductRequest, UpdateProductRequest } from "../dto/product.dto";
 
 const router = express.Router();
 
@@ -10,41 +12,41 @@ export const catalogService = new CatalogService(new CatalogRepository());
 router.post(
   "/products",
   async (req: Request, res: Response, next: NextFunction) => {
-    // try {
-    //   const { errors, input } = await RequestValidator(
-    //     CreateProductRequest,
-    //     req.body
-    //   );
+    try {
+      const { errors, input } = await RequestValidator(
+        CreateProductRequest,
+        req.body
+      );
 
-    //   if (errors) return res.status(400).json(errors);
-    //   const data = await catalogService.createProduct(input);
-    //   return res.status(201).json(data);
-    // } catch (error) {
-    //   const err = error as Error;
-    //   return res.status(500).json(err.message);
-    // }
+      if (errors) return res.status(400).json(errors);
+      const data = await catalogService.createProduct(input);
+      return res.status(201).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
   }
 );
 
 router.patch(
   "/products/:id",
   async (req: Request, res: Response, next: NextFunction) => {
-    // try {
-    //   const { errors, input } = await RequestValidator(
-    //     UpdateProductRequest,
-    //     req.body
-    //   );
+    try {
+      const { errors, input } = await RequestValidator(
+        UpdateProductRequest,
+        req.body
+      );
 
-    //   const id = parseInt(req.params.id) || 0;
+      const id = parseInt(req.params.id) || 0;
 
-    //   if (errors) return res.status(400).json(errors);
+      if (errors) return res.status(400).json(errors);
 
-    //   const data = await catalogService.updateProduct({ id, ...input });
-    //   return res.status(200).json(data);
-    // } catch (error) {
-    //   const err = error as Error;
-    //   return res.status(500).json(err.message);
-    // }
+      const data = await catalogService.updateProduct({ id, ...input });
+      return res.status(200).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
   }
 );
 
